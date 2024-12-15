e508fa800dd68e3ec5c60e9210155f3c1e6434c2
Rajnish Kumar <rajnish@Rajnishs-MacBook-Pro.local>
2024-12-15 13:11:13 +0530

    Initial project structure

---
+++ src/main/kotlin/com/azure/az204/Az204Application.kt
@@ -0,0 +1,11 @@
+package com.azure.az204
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Az204Application
+
+fun main(args: Array<String>) {
+	runApplication<Az204Application>(*args)
+}
---
+++ src/test/kotlin/com/azure/az204/Az204ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.azure.az204
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Az204ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
